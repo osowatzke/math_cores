c06f0af93bd99a450ae19a5f68023af4c005df73
Owen <osowatzke@gmail.com>
2026-09-19 22:21:56 -0700

    Getting test to pass with 32x32 mult

---
+++ tb_compare.v
@@ -0,0 +1,26 @@
+module tb_compare #(
+    parameter WIDTH = 32) (
+    input  wire clkIn,
+    input  wire rstIn,
+    input  wire validIn,
+    input  wire [WIDTH - 1:0] refIn,
+    input  wire [WIDTH - 1:0] measIn,
+    output wire errOut);
+    
+    reg errR;
+    
+    always @(posedge clkIn) begin
+        if (rstIn == 1) begin
+            errR <= 0;
+        end else begin
+            if ((validIn == 1) && (refIn !== measIn)) begin
+                $display("Mismatch at time %t. Meas (0x%016X) != Ref (0x%016X)", $realtime, measIn, refIn);
+                errR <= 1;
+            end
+        end
+    end
+    
+    assign errOut = errR;
+
+endmodule
+    
--- unsigned_mult_tb.v
+++ unsigned_mult_tb.v
@@ -19,20 +19,27 @@ module unsigned_mult_tb;
     
     reg [31:0] aR;
     reg [31:0] bR;
+    reg validR;
     
     always @(posedge clk) begin
         if (rst == 1) begin
             aR  <= 0;
             bR  <= 0;
+            validR <= 0;
         end else begin
             aR  <= $random;
             bR  <= $random;
+            validR <= 1;
         end
     end
     
-    wire [63:0] yRef, yMeas;
+    wire [63:0] y, yRef, yMeas;
+    wire validPipe;
     
-    assign yRef = {{32{1'b0}}, aR} * {{32{1'b0}}, bR};
+    assign y = {{32{1'b0}}, aR} * {{32{1'b0}}, bR};
+    
+    pipe #(.DELAY(7), .WIDTH(64)) pipe_y    (.clkIn(clk), .rstIn(rst), .xIn(y),      .xOut(yRef));
+    pipe #(.DELAY(7), .WIDTH(1))  pipe_valid(.clkIn(clk), .rstIn(rst), .xIn(validR), .xOut(validPipe));
     
     unsigned_mult #(
         .A_WIDTH(32),
@@ -43,5 +50,28 @@ module unsigned_mult_tb;
         .aIn  (aR),
         .bIn  (bR),
         .yOut (yMeas));
+        
+    wire err;
+    
+    tb_compare #(
+        .WIDTH(64))
+    comp_i (
+        .clkIn(clk),
+        .rstIn(rst),
+        .validIn(validPipe),
+        .refIn(yRef),
+        .measIn(yMeas),
+        .errOut(err));
+    
+    initial begin
+        $timeformat(-9, 3, " ns", 10);
+        #10000
+        if (err) begin
+            $display("One or More Tests Failed :(");
+        end else begin
+            $display("All Tests Passed :)");
+        end
+        $finish();
+    end 
     
 endmodule
